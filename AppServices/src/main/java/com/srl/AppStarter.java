57ef304458f61d02bc0ca48d5db6f4a471d02a29
Sairam Prasad Vanamala <s.vanamala@walmart.com>
2021-05-31 12:36:44 -0500

    first commit

---
+++ AppServices/src/main/java/com/srl/AppStarter.java
@@ -0,0 +1,14 @@
+package com.srl;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStarter.class, args);
+	}
+
+}
